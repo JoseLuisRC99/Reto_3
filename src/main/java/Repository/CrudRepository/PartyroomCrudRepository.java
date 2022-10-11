e7b03af00378d2431cebea374ca51a2b7e5eea01
Jose Rodriguez <jos999rodri@gmail.com>
2022-10-11 12:35:33 -0500

    comentario

---
+++ src/main/java/Repository/CrudRepository/PartyroomCrudRepository.java
@@ -0,0 +1,7 @@
+package Repository.CrudRepository;
+
+import Model.Partyroom;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartyroomCrudRepository extends CrudRepository<Partyroom, Integer> {
+}
---
+++ src/main/java/com/example/reto_c3/RetoC3Application.java
@@ -0,0 +1,13 @@
+package com.example.reto_c3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoC3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoC3Application.class, args);
+    }
+
+}
